9b9ae344f1f0c4eedf072d35c2e66b1570447a34
Sandai64 <erwan.egasse@outlook.com>
2025-04-29 14:37:06 +0200

    fix font size

--- src/components/ForYou.vue
+++ src/components/ForYou.vue
@@ -5,9 +5,9 @@ const countries = CustomConsts.COUNTRIES
 <template>
     <div class="px-8">
         <p class="mb-4 font-extrabold text-4xl">#ForYou</p>
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-3 gap-4">
             <div v-for="country in countries" class="text-lg shadow rounded-md outline overflow-hidden hover:brightness-75 transition-all cursor-pointer">
-                <span class="bg-neutral-800 text-neutral-200 px-4 py-3">{{ country.sc }}</span>
+                <span class="font-mono bg-neutral-800 text-neutral-200 px-4 py-3">{{ country.sc }}</span>
                 <span class="px-4 py-2">{{ country.name }}</span>
             </div>
         </div>
