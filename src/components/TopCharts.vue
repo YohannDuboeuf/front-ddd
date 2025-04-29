a4db64105ffd6e74ee37b5ab9ffafdd9dcb1eb0b
Sandai64 <erwan.egasse@outlook.com>
2025-04-29 14:37:17 +0200

    topcharts: fix font size & padding

--- src/components/TopCharts.vue
+++ src/components/TopCharts.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="px-8">
+    <div class="px-8 pb-8">
         <p class="font-extrabold text-4xl mb-4">#TopCharts</p>
         <p class="font-semibold text-neutral-600 mb-1">By country</p>
-        <div class="grid grid-cols-4 gap-4">
+        <div class="grid grid-cols-3 gap-4">
             <div v-for="country in countries" class="text-lg shadow rounded-md outline overflow-hidden hover:brightness-75 transition-all cursor-pointer">
                 <RouterLink :to="`/top-charts/${country.sc}`">
-                    <span class="bg-neutral-800 text-neutral-200 px-4 py-3">{{ country.sc }}</span>
+                    <span class="bg-neutral-800 font-mono text-neutral-200 px-4 py-3">{{ country.sc }}</span>
                     <span class="px-4 py-2 w-full">{{ country.name }}</span>
                 </RouterLink>
             </div>
