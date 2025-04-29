dcf7445cc290453cfe8cd770c378007d21c2817b
Sandai64 <erwan.egasse@outlook.com>
2025-04-29 10:48:29 +0200

    foryou: add base dependency

--- src/components/ForYou.vue
+++ src/components/ForYou.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+import CustomConsts from './consts.ts'
+const countries = CustomConsts.COUNTRIES
+</script>
 <template>
     <div class="px-8">
-        <p class="font-extrabold text-4xl">#ForYouPage</p>
+        <p class="mb-4 font-extrabold text-4xl">#ForYou</p>
+        <div class="grid grid-cols-4 gap-4">
+            <div v-for="country in countries" class="text-lg shadow rounded-md outline overflow-hidden hover:brightness-75 transition-all cursor-pointer">
+                <span class="bg-neutral-800 text-neutral-200 px-4 py-3">{{ country.sc }}</span>
+                <span class="px-4 py-2">{{ country.name }}</span>
+            </div>
+        </div>
     </div>
 </template>
-<script></script>
